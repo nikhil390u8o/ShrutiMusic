1e064cadc91222289b01505b8b23fe29c47a2840
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-30 15:48:04 +0530

    Update __main__.py to __main__.py

--- ShrutiMusic/__main__.py
+++ ShrutiMusic/__main__.py
@@ -20,6 +20,7 @@
 # Email: badboy809075@gmail.com
 
 
+
 import asyncio
 import importlib
 from pyrogram import idle
@@ -27,7 +28,7 @@ from pyrogram.types import BotCommand
 from pytgcalls.exceptions import NoActiveGroupCall
 import config
 from ShrutiMusic import LOGGER, app, userbot
-from ShrutiMusic.core.call import Aviax  # <- fixed here
+from ShrutiMusic.core.call import Aviax
 from ShrutiMusic.misc import sudo
 from ShrutiMusic.plugins import ALL_MODULES
 from ShrutiMusic.utils.database import get_banned_users, get_gbanned
@@ -118,23 +119,14 @@ COMMANDS = [
 ]
 
 async def setup_bot_commands():
-    """Setup bot commands during startup"""
     try:
-        # Set bot commands
         await app.set_bot_commands(COMMANDS)
         LOGGER("ShrutiMusic").info("Bot commands set successfully!")
-        
     except Exception as e:
         LOGGER("ShrutiMusic").error(f"Failed to set bot commands: {str(e)}")
 
 async def init():
-    if (
-        not config.STRING1
-        and not config.STRING2
-        and not config.STRING3
-        and not config.STRING4
-        and not config.STRING5
-    ):
+    if not any([config.STRING1, config.STRING2, config.STRING3, config.STRING4, config.STRING5]):
         LOGGER(__name__).error("Assistant client variables not defined, exiting...")
         exit()
 
@@ -147,36 +139,34 @@ async def init():
         users = await get_banned_users()
         for user_id in users:
             BANNED_USERS.add(user_id)
-    except:
-        pass
+    except Exception as e:
+        LOGGER("ShrutiMusic").warning(f"Failed to load banned users: {e}")
 
     await app.start()
-    
-    # Setup bot commands during startup
     await setup_bot_commands()
 
     for all_module in ALL_MODULES:
-        importlib.import_module("ShrutiMusic.plugins" + all_module)
+        importlib.import_module(f"ShrutiMusic.plugins.{all_module}")
 
     LOGGER("ShrutiMusic.plugins").info("Successfully Imported Modules...")
 
     await userbot.start()
-    await Aviax.start()  # <- fixed here
+    await Aviax.start()
 
     try:
-        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")  # <- fixed here
+        await Aviax.stream_call("https://te.legra.ph/file/29f784eb49d230ab62e9e.mp4")
     except NoActiveGroupCall:
         LOGGER("ShrutiMusic").error(
-            "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
+            "Please turn on the videochat of your log group/channel.\nStopping Bot..."
         )
         exit()
-    except:
+    except Exception:
         pass
 
-    await Aviax.decorators()  # <- fixed here
+    await Aviax.decorators()
 
     LOGGER("ShrutiMusic").info(
-        "\x53\x68\x72\x75\x74\x69\x20\x4d\x75\x73\x69\x63\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\x0a\x0a\x44\x6f\x6e\x27\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x53\x68\x72\x75\x74\x69\x42\x6f\x74\x73"
+        "Shruti Music Started Successfully.\n\nDon't forget to visit @ShrutiBots"
     )
 
     await idle()
