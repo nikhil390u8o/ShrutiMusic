c0f25a7208114c76ef7ca59f0407832ef15042d9
nikhil390u8o <sangeetasharma1005sa@gmail.com>
2025-08-30 16:56:18 +0530

    Update __main__.py to __main__.py

--- ShrutiMusic/__main__.py
+++ ShrutiMusic/__main__.py
@@ -1,26 +1,3 @@
-# Copyright (c) 2025 Nand Yaduwanshi <NoxxOP>
-# Location: Supaul, Bihar
-#
-# All rights reserved.
-#
-# This code is the intellectual property of Nand Yaduwanshi.
-# You are not allowed to copy, modify, redistribute, or use this
-# code for commercial or personal projects without explicit permission.
-#
-# Allowed:
-# - Forking for personal learning
-# - Submitting improvements via pull requests
-#
-# Not Allowed:
-# - Claiming this code as your own
-# - Re-uploading without credit or permission
-# - Selling or using commercially
-#
-# Contact for permissions:
-# Email: badboy809075@gmail.com
-
-
-
 import asyncio
 import importlib
 from pyrogram import idle
@@ -145,9 +122,11 @@ async def init():
     await app.start()
     await setup_bot_commands()
 
+    # ✅ Skip empty modules
     for all_module in ALL_MODULES:
-        importlib.import_module(f"ShrutiMusic.plugins.{all_module}")  # ✅ FIXED
-
+        if not all_module:
+            continue
+        importlib.import_module(f"ShrutiMusic.plugins.{all_module}")
 
     LOGGER("ShrutiMusic.plugins").info("Successfully Imported Modules...")
 
